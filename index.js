c68d3fc17850273a0bffa3c1c1e9e5cc7f1506c9
pablo128482 <ashley.c.clay@gmail.com>
2023-07-11 16:01:44 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(
+      (driver) => driver.toLowerCase() === string.toLowerCase()
+    );
+  }
+  function fuzzyMatch(drivers, string) {
+    return drivers.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(string.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(drivers, string) {
+    return drivers.filter(record => record.name === string);
+  }
